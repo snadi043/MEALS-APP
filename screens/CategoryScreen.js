7c9ba1bcd5af69b6c8056bc6a888e40f320eaf09
snadi043 <snadi043@uottawa.ca>
2024-12-13 12:01:46 -0500

    Directory structure refactored.

--- screens/CategoryScreen.js
+++ screens/CategoryScreen.js
@@ -1,6 +1,6 @@
 import { FlatList, Text } from "react-native";
 import { CATEGORIES } from "../data/dummy_data";
-import CategoryListGrid from "./components/CategoryListGrid";
+import CategoryListGrid from "../components/CategoryListGrid";
 
 function renderItemCategoryList(itemData){
     return <CategoryListGrid title={itemData.item.title} color={itemData.item.color}/>
